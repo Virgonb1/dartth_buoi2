457cf56c69fd75bdb76b9a7cd52b4ce65f8d9afd
Virgonb1 <118275437+Virgonb1@users.noreply.github.com>
2022-12-19 22:23:08 +0700

    first commit

---
+++ bin/dartth_412.dart
@@ -0,0 +1,5 @@
+import 'package:dartth_buoi2/dartth_buoi2.dart' as dartth_buoi2;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dartth_buoi2.calculate()}!');
+}
---
+++ test/dartth_buoi2_test.dart
@@ -0,0 +1,8 @@
+import 'package:dartth_buoi2/dartth_buoi2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
